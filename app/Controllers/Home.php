62294ff590c9f75fa53cc657f1b06a1ac26df821
tiostwn <satriyo.stwn@gmail.com>
2023-11-26 12:33:35 +0700

    add: files

---
+++ app/Controllers/Home.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controllers;
+
+class Home extends BaseController
+{
+    public function index(): string
+    {
+        return view('welcome_message');
+    }
+
+    public function helloWorld(): string
+    {
+        return view('hello_world');
+    }
+}
+
---
+++ app/Views/hello_world.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello World</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+    <div class="h-screen flex flex-col items-center justify-center">
+        <h2 cLass="text-sky-600 font-bold text-[3rem]">Hello World</h2>
+        <p class="text-slate-500 font-reguler text-small">Hanafi Satriyo Utomo Setiawan - 50252111965</p>
+    </div>
+    
+</body>
+</html>
+
